9c51e938545cc7b3ff7618042a4b69ae47a5277a
yfby <harveykyleybanez@gmail.com>
2026-07-19 23:46:05 +0800

    feat: separated logic

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,11 @@ use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
     DefaultTerminal, Frame,
     buffer::Buffer,
-    layout::{Constraint, Direction, Layout, Rect},
-    style::Stylize,
-    symbols::border,
-    text::{Line, Text},
-    widgets::{Block, Paragraph, Widget},
+    layout::{Alignment, Constraint, Layout, Rect},
+    style::{Color, Style},
+    widgets::{Block, List, ListItem, ListState, StatefulWidget, Widget},
 };
-use std::io;
+use std::{io, u8};
 
 mod sample;
 mod task;
@@ -23,21 +21,36 @@ fn main() -> Result<()> {
 
 struct App {
     exit: bool,
-    current_screen: CurrentScreen,
+    current_layout: CurrentLayout,
+    current_interface: CurrentInterface,
     task_collection: task::TaskListCollection,
 }
 
-pub enum CurrentScreen {
-    ListMenu,
-    ListBody,
+enum CurrentLayout {
+    Task,
     Exit,
+}
+
+enum CurrentInterface {
+    TaskMenu,
+    TaskBody,
+    Write,
+    Exit,
+}
+
+struct Write {
+    item_index: u8,
+    input: String,
+    interface: CurrentInterface,
+    character_index: u8,
 }
 
 impl Default for App {
     fn default() -> Self {
         Self {
             exit: false,
-            current_screen: CurrentScreen::ListMenu,
+            current_layout: CurrentLayout::Task,
+            current_interface: CurrentInterface::TaskMenu,
             task_collection: task::load_or_default(SAVE_FILE),
         }
     }
@@ -53,11 +66,7 @@ impl App {
     }
 
     fn draw(&self, frame: &mut Frame) {
-        let layout = Layout::default()
-            .direction(Direction::Horizontal)
-            .constraints(vec![Constraint::Percentage(25), Constraint::Percentage(75)])
-            .split(frame.area());
-        frame.render_widget("H000O", layout[1]);
+        frame.render_widget(self, frame.area());
     }
 
     fn handle_events(&mut self) -> io::Result<()> {
@@ -69,9 +78,24 @@ impl App {
         };
         Ok(())
     }
+}
 
+/// Event Logic
+impl App {
     fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match self.current_interface {
+            CurrentInterface::TaskMenu => self.key_event_task_menu(key_event),
+            CurrentInterface::TaskBody => todo!(),
+            CurrentInterface::Write => todo!(),
+            CurrentInterface::Exit => todo!(),
+        }
+    }
+
+    fn key_event_task_menu(&mut self, key_event: KeyEvent) {
         match key_event.code {
+            KeyCode::Char('a') => {
+                todo!()
+            }
             KeyCode::Char('q') => self.exit(),
             KeyCode::Char('w') => self.save(),
             _ => {}
@@ -89,8 +113,47 @@ impl App {
     }
 }
 
-impl Widget for &mut App {
+/// Rendering Logic
+impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        todo!()
+        match self.current_layout {
+            CurrentLayout::Task => self.render_list(area, buf),
+            CurrentLayout::Exit => todo!(),
+        }
+    }
+}
+
+impl App {
+    fn render_list(&self, area: Rect, buf: &mut Buffer) {
+        let chunks = Layout::horizontal([Constraint::Percentage(20), Constraint::Percentage(80)])
+            .split(area);
+
+        let list_menu_chunk = chunks[0];
+        let list_body_chunk = chunks[1];
+
+        let list_menu =
+            Layout::vertical([Constraint::Length(3), Constraint::Fill(1)]).split(list_menu_chunk);
+        let list_body =
+            Layout::vertical([Constraint::Length(3), Constraint::Fill(1)]).split(list_body_chunk);
+
+        let items: Vec<ListItem> = self
+            .task_collection
+            .lists()
+            .iter()
+            .map(|tl| ListItem::from(tl.name().to_string()))
+            .collect();
+
+        let list = List::new(items)
+            .block(
+                Block::bordered()
+                    .title("Task Menu")
+                    .title_alignment(Alignment::Center),
+            )
+            .highlight_style(Style::default().fg(Color::Black).bg(Color::Yellow))
+            .highlight_symbol(">> ");
+
+        let mut list_state = ListState::default().with_selected(Some(0));
+
+        StatefulWidget::render(list, list_menu[1], buf, &mut list_state);
     }
 }
